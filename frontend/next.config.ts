8e1bd4529627fcd2db929434357a6fd4ac9eee6c
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2026-01-05 01:20:16 +0530

    config change to use .env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,23 @@
 import type { NextConfig } from "next";
 
+const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+if (!NEXT_PUBLIC_API_URL) {
+  throw new Error("NEXT_PUBLIC_API_URL is not defined");
+}
+
 const nextConfig: NextConfig = {
   reactCompiler: true,
 
   async rewrites() {
     return [
-      // PDFs + HTML reports
       {
         source: "/reports/:path*",
-        destination: "http://localhost:8000/reports/:path*",
+        destination: `${NEXT_PUBLIC_API_URL}/reports/:path*`,
       },
-
-      // API calls
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*",
+        destination: `${NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
